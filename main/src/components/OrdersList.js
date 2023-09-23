bb54393f078db28caeecad3c057d3d6fd8d29871
pedrojaoulack <60972832+pedrojaoulack@users.noreply.github.com>
2023-09-23 18:18:24 -0300

    Update OrdersList.js criei botão de excluir ordem

--- main/src/components/OrdersList.js
+++ main/src/components/OrdersList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import OrderItem from './OrderItem';
 
-const OrdersList = ({ orders }) => {
+const OrdersList = ({ orders, removeOrder }) => {
   return (
     <div className='container cart-itens'>
-      <div  className='text-aligns'>
+      <div className='text-aligns'>
         {orders.map((order, index) => (
-          <OrderItem key={index} order={order} />
+          <OrderItem key={index} order={order} removeOrder={removeOrder} />
         ))}
       </div>
     </div>
