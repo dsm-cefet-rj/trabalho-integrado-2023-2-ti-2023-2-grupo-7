4b35e16160c42ca573b76814b614ae7191b803e9
pedrojaoulack <60972832+pedrojaoulack@users.noreply.github.com>
2023-09-23 19:12:46 -0300

    Update OrdersList.js atualizei para incluir mudança de status do pedido

--- main/src/components/OrdersList.js
+++ main/src/components/OrdersList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import OrderItem from './OrderItem';
 
-const OrdersList = ({ orders, removeOrder }) => {
+const OrdersList = ({ orders, removeOrder, updateOrderStatus }) => {
   return (
     <div className='container cart-itens'>
       <div className='text-aligns'>
         {orders.map((order, index) => (
-          <OrderItem key={index} order={order} removeOrder={removeOrder} />
+          <OrderItem key={index} order={order} removeOrder={removeOrder} updateOrderStatus={updateOrderStatus} />
         ))}
       </div>
     </div>
