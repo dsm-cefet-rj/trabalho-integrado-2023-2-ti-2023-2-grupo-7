ff9d7c67853b864c323fe6fc200d695be1f3249b
andressa-oliveira21051 <andressaoliveira.cefetrj@gmail.com>
2023-11-12 21:54:42 -0300

    Voltando ao estado funcional

--- backend/Server.js
+++ backend/Server.js
@@ -1,30 +1,82 @@
-// server.js
-const cartRoutes = require('./routes/CartRoutes');
-const productRoutes = require('./routes/ProductRoutes');
-const userRoutes = require('./routes/UserRoutes');
-const orderRoutes = require('./routes/OrderRoutes'); // Importe as rotas de pedidos
+const cartRoutes = require('./Routes/CartRoutes');
+const productRoutes = require('./Routes/ProductsRoutes');
+const UserRoutes = require('./Routes/UserRoutes');
+const authRoutes = require('./Routes/authRoutes');
+
+const cookieParser = require('cookie-parser');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3001;
-const cors = require('cors');
+const cors = require("cors");
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 app.use(cors({ origin: '*', credentials: false }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(
-  'mongodb+srv://user:user@cluster0.mehdhep.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true }
-);
+mongoose.connect('mongodb+srv://user:user@cluster0.mehdhep.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+const UserModel = require('./Models/UserModel');
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email', 
+    passwordField: 'password', 
+  },
+  async (email, password, done) => {
+    try {
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return done(null, false, { message: 'Credenciais do usuario inválidas' });
+      }
+
+      if (await user.comparePassword(password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Credenciais inválidas' }); 
+      }
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'ALWAYS1234567809',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 3600000 }}
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await UserModel.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
 
 app.use('/', cartRoutes);
 app.use('/', productRoutes);
-app.use('/', userRoutes);
-app.use('/', orderRoutes); // Utilize as rotas de pedidos
+app.use('/', UserRoutes);
+app.use('/', authRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
